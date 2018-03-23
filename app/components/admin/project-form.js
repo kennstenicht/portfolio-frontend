910c5b8fc8780f4e5997b46b6dd47b031a6fbeec
Christoph <mussichhaben@kennstenicht.de>
2018-03-23 17:28:32 +0100

    Refactor(ProjectForm): Use routing service insted of route actions

--- app/components/admin/project-form.js
+++ app/components/admin/project-form.js
@@ -1,10 +1,21 @@
 import Component from '@ember/component';
+import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  // Services
+  router: service(),
+
+  // Actions
   actions: {
     save(model) {
-      model.save();
-      this.sendAction('saved');
-    }
+      model.save().then(this._transitionToProjects.bind(this));
+    },
+
+  },
+
+  // Privat functions
+  _transitionToProjects() {
+    get(this, 'router').transitionTo('admin.projects');
   }
 });
