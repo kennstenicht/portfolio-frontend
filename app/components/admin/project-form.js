6ee585f66ba38d3dfcdbe7838a385ec08b2fc37a
Christoph <mussichhaben@kennstenicht.de>
2018-03-24 12:57:41 +0100

    Feat(ProjectForm): Throw error message

--- app/components/admin/project-form.js
+++ app/components/admin/project-form.js
@@ -9,16 +9,24 @@ export default Component.extend({
   // Actions
   actions: {
     save(model) {
-      model.save().then(this._transitionToProjects.bind(this));
+      model.save()
+        .then(this._transitionToProjects.bind(this))
+        .catch(this._throwError.bind(this));
     },
 
     deleteRecord(model) {
-      model.destroyRecord().then(this._transitionToProjects.bind(this));
+      model.destroyRecord()
+        .then(this._transitionToProjects.bind(this));
     },
   },
 
   // Privat functions
   _transitionToProjects() {
     get(this, 'router').transitionTo('admin.projects');
+  },
+
+  _throwError(reason) {
+    let error = reason.errors || reason;
+    console.log(error);
   }
 });
