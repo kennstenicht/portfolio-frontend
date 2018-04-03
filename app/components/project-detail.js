68941ed339a9d2cb65be7cbc47fb9cdbe1a89c80
Christoph <mussichhaben@kennstenicht.de>
2018-04-03 18:21:56 +0200

    Style(ProjectDetail): Add BEM and basic styling

--- app/components/project-detail.js
+++ app/components/project-detail.js
@@ -1,8 +1,13 @@
 import Component from '@ember/component';
 import { get, computed } from '@ember/object';
 import { getOwner } from '@ember/application';
+import BEM from 'ember-cli-bem/mixins/bem';
 
-export default Component.extend({
+export default Component.extend(BEM, {
+  // Defaults
+  tagName: 'article',
+  blockName: 'c-project-detail',
+
   // Computed properties
   projectTemplate: computed('project.slug', function () {
     let slug = get(this,'project.slug');
