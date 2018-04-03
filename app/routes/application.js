fd39e59474be03500f45ea6d3e8cdec47e58f666
Christoph <mussichhaben@kennstenicht.de>
2018-04-03 17:48:13 +0200

    Feat(EmberIntel): Add ember-intl and translate all strings

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { get } from '@ember/object';
 import ScrollControllerRouteMixin from 'ember-scrollmagic/mixins/scroll-controllers/route-mixin';
 import SetHeadTags from '../mixins/set-head-tags';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
@@ -7,11 +8,17 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 export default Route.extend(ApplicationRouteMixin, ScrollControllerRouteMixin, SetHeadTags, {
   // Services
   session: service(),
+  intl: service(),
 
   // Variables
   metaTitle: 'A berlin based Front-End Engenieer & UI/UX-Designer | Christoph Wiedenmann',
   metaDescription: 'ag-prop ist ein Berliner Digitalstudio. Für unsere Kunden entwickeln wir digitale Produkte, Services und Kommunikationsstrategien.',
   metaImage: 'images/default_sharing.jpg',
+
+  beforeModel() {
+    // TODO: Add language switch
+    get(this, 'intl').setLocale('de');
+  },
 
   // Actions
   actions: {
