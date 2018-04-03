82ada70fba440e0cfaca4a14b84ddecf1e33f6a9
Christoph <mussichhaben@kennstenicht.de>
2018-04-03 16:10:26 +0200

    Feat(ProjectSummary): Add basic project summary component

---
+++ app/components/project/project-summary.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import BEM from 'ember-cli-bem/mixins/bem';
+
+export default Component.extend(BEM, {
+  // Class bindings
+  blockName: 'c-project-summary',
+});
