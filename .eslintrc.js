18b336f07a0edd2e741726f855c15ea3c9db327f
Christoph <christoph@ag-prop.com>
2018-10-19 18:12:11 +0200

    Chore(Update): Update to ember 3.5 and all packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,8 +20,10 @@ module.exports = {
     // node files
     {
       files: [
-        'testem.js',
+        '.template-lintrc.js',
         'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
