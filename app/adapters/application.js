d3271d02af36e3ee81854b5a3183596f8a0aee04
Christoph <christoph@ag-prop.com>
2019-07-15 21:43:59 +0200

    Feat(Adapter): Add dynamic locale and host

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,36 @@
 import DS from 'ember-data';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
+import ENV from 'portfolio/config/environment';
 
-export default class ApplicationAdapter extends DS.JSONAPIAdapter.extend(AdapterFetch, DataAdapterMixin) {
-  // Defaults
-  namespace = 'api/v1';
+export default class ApplicationAdapter extends DS.JSONAPIAdapter.extend(
+  DataAdapterMixin
+) {
+  // Services
+  @service intl;
+  @service session;
+  @service fastboot;
+
+
+  // Computed properties
+  @computed('fastboot.request.{protocol,host}')
+  get host() {
+    let protocol = ENV.environment !== 'development' ? 'https' : 'http';
+
+    if (ENV.environment !== 'development' && this.fastboot.isFastBoot) {
+      return protocol + '://' + this.fastboot.request.host;
+    } else {
+      return null;
+    }
+  }
+
+  @computed('session.data.locale')
+  get namespace() {
+    let locale = this.intl.locale || 'de';
+
+    return [locale, 'v1'].join('/');
+  }
 
   // Hooks
   authorize(xhr) {
