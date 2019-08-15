27b5639ed09d2c415a1e381928cfa610e6316099
Christoph <christoph@ag-prop.com>
2019-08-15 20:48:47 +0200

    Fix(Application): Fix application classes

--- app/components/application.js
+++ app/components/application.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { className } from '@ember-decorators/component';
 
 export default class AppLicationComponent extends Component {
   // Services
@@ -13,26 +12,12 @@ export default class AppLicationComponent extends Component {
 
 
   // Computed Properties
-  @className
   @computed('router.currentURL')
-  get currentRouteClass() {
-    let routeSegments = this.router.currentURL.substring(1).split('/');
-    let routeName = this.router.currentRouteName.replace('.', '-');
-    let blockName = this.blockName;
-    let classes = new Array();
-
-    routeSegments.forEach(function (segement) {
-      if(segement) {
-        classes.push(blockName + '--' + segement);
-      } else {
-        classes.push(blockName + '--home');
-      }
-    });
-
-    if(classes.indexOf(blockName + '--' + routeName) == -1) {
-      classes.push(blockName + '--' + routeName);
-    }
-
-    return classes.join(' ');
+  get urlSegments() {
+    return this.router
+      .currentURL
+      .substring(1)
+      .split('/')
+      .filter(n => n);
   }
 }
