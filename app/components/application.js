a8df3cb0b2df3cdc825d621cbc73e67113b2b23b
Christoph <christoph@ag-prop.com>
2020-02-07 18:45:05 +0100

    Feat(CookieNotice): Add cookie notice

--- app/components/application.js
+++ app/components/application.js
@@ -1,23 +1,57 @@
 import Component from '@glimmer/component';
-import { computed } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class AppLicationComponent extends Component {
   // Services
   @service router;
+  @service fastboot;
   @service flashMessages;
 
+
   // Defaults
-  block = 'c-application';
+  blockName = 'c-application';
+  @tracked showCookieNotice = false;
 
 
-  // Computed Properties
-  @computed('router.currentURL')
+  // Getter and setter
   get urlSegments() {
     return this.router
       .currentURL
       .substring(1)
       .split('/')
       .filter(n => n);
+  }
+
+
+  // Hooks
+  constructor() {
+    super(...arguments);
+
+    if (!this.fastboot.isFastBoot) {
+      window.addEventListener('hashchange', this.checkHash.bind(this), false);
+    }
+  }
+
+  willDestroy() {
+    if (!this.fastboot.isFastBoot) {
+      window.removeEventListener('hashchange', this.checkHash.bind(this), true);
+    }
+  }
+
+
+  checkHash() {
+    if (location.hash == '#change-cookie-settings') {
+      this.toggleCookieNotice();
+
+      location.hash = ''
+    }
+  }
+
+  // Actions
+  @action
+  toggleCookieNotice() {
+    this.showCookieNotice = !this.showCookieNotice;
   }
 }
---
+++ app/components/application/cookie-notice.js
@@ -0,0 +1,69 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class ApplicationCookieNoticeComponent extends Component {
+  // Services
+  @service cookies;
+
+
+  // Defaults
+  blockName = "c-application-cookie-notice"
+
+
+  // Hooks
+  constructor() {
+    super(...arguments);
+
+    if (!this.cookies.exists('hide_cookie_notice')) {
+      this.args.toggleCookieNotice();
+    }
+  }
+
+
+  // Getter and setter
+  get allowAnalyseCookies() {
+    return this.getCookieWithFallback('allow_analyse_cookies', false);
+  }
+
+  set allowAnalyseCookies(value) {
+    this.cookies.write('allow_analyse_cookies', value);
+  }
+
+  get allowMarketingCookies() {
+    return this.getCookieWithFallback('allow_marketing_cookies', false);
+  }
+
+  set allowMarketingCookies(value) {
+    this.cookies.write('allow_marketing_cookies', value);
+  }
+
+
+  // Actions
+  @action
+  allowSelectedCookies() {
+    this.saveSettings();
+  }
+
+  @action
+  allowAllCookies() {
+    this.allowAnalyseCookies = true;
+
+    this.saveSettings();
+  }
+
+
+  // Functions
+  getCookieWithFallback(cookie, fallback) {
+    if (!this.cookies.exists(cookie)) {
+      this.cookies.write(cookie, fallback);
+    }
+
+    return this.cookies.read(cookie) == 'true';
+  }
+
+  saveSettings() {
+    this.cookies.write('hide_cookie_notice', true);
+    this.args.toggleCookieNotice();
+  }
+}
