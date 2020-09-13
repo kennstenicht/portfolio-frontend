55581520c03d00cbec9e833312accb79bf0fe43c
Christoph <christoph@ag-prop.com>
2020-09-13 09:02:21 +0200

    Chore(Ember): Update to 3.21.2

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'portfolio/app';
+import config from 'portfolio/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import './helpers/flash-message';
