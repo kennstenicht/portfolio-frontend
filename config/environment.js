cdbde8207d35c099f98a8a0b1d2de52daee8c66d
Christoph <christoph@ag-prop.com>
2020-09-13 16:49:54 +0200

    Chore(Pods): Use pods for routes

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'portfolio',
+    podModulePrefix: 'portfolio/routes',
     environment,
     rootURL: '/',
     locationType: 'auto',
