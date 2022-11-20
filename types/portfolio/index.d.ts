fd08134944bbfbd3ebf048458aca85a090575745
kennstenicht <mussichhaben@kennstenicht.de>
2022-11-20 14:43:57 +0100

    Fix(Typescript): Remove preview types

--- types/portfolio/index.d.ts
+++ types/portfolio/index.d.ts
@@ -1,4 +1,8 @@
-import 'ember-source/types';
-import 'ember-source/types/preview';
+import Ember from 'ember';
+
+declare global {
+  interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
+  // interface Function extends Ember.FunctionPrototypeExtensions {}
+}
 
 export {};
