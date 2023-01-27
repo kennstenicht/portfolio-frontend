7dd4f60076b42e3dbdf31f96cdf374b9583c55da
kennstenicht <mussichhaben@kennstenicht.de>
2023-01-27 15:41:25 +0100

    Refactor(RenderModifier): Replace with custom modifiers

--- types/@ember/render-modifiers/modifiers/did-update.d.ts
+++
@@ -1,6 +0,0 @@
-import { RenderModifier as DidUpdateModifier } from './-private/shared';
-
-declare module '@ember/render-modifiers/modifiers/did-update' {
-  export { DidUpdateModifier };
-  export default DidUpdateModifier;
-}
--- types/@ember/render-modifiers/modifiers/will-destroy.d.ts
+++
@@ -1,6 +0,0 @@
-import { RenderModifier as WillDestroyModifier } from './-private/shared';
-
-declare module '@ember/render-modifiers/modifiers/will-destroy' {
-  export { WillDestroyModifier };
-  export default WillDestroyModifier;
-}
