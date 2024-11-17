aa2bec812a33dd9546c5a6cb006f3672ca940a95
kennstenicht <mussichhaben@kennstenicht.de>
2024-11-17 14:03:58 +0100

    Chore(EmberData): Update to 5.4.0-alpha

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -9,6 +9,11 @@ module.exports = function (defaults) {
     babel: {
       sourceMaps: 'inline',
     },
+    emberData: {
+      deprecations: {
+        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
