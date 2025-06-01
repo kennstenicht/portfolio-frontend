2d0b7c5cf5a3ea80b7ca0d8e1402f437087a8732
Christoph Wiedenmann <mussichhaben@kennstenicht.de>
2025-06-01 12:29:26 +0200

    Fix(Store): Remove store service

--- app/services/store.ts
+++
@@ -1,7 +0,0 @@
-import BaseStore from '@ember-date/store';
-import { service } from '@ember/service';
-import RequestManager from './request-manager';
-
-export default class Store extends BaseStore {
-  @service declare requestManager: RequestManager;
-}
