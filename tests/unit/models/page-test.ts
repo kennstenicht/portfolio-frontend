76bad48c4413cb96e12378a0d4e83a00d2715a2f
kennstenicht <mussichhaben@kennstenicht.de>
2026-07-27 20:36:34 +0200

    Feat(TypeScript): Update alle files to ts

--- tests/unit/models/page-test.ts
+++ tests/unit/models/page-test.ts
@@ -1,3 +1,4 @@
+import type Store from '@ember-data/store';
 import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
@@ -5,8 +6,8 @@ module('Unit | Model | page', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    const store = this.owner.lookup('service:store');
-    const model = store.createRecord('page');
+    const store = this.owner.lookup('service:store') as unknown as Store;
+    const model = store.createRecord('page', {});
 
     assert.ok(model);
   });
