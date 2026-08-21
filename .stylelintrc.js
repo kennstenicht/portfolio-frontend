38708d0f03b9ef4d4bdae7be7c6596572d97039a
kennstenicht <mussichhaben@kennstenicht.de>
2026-08-21 10:07:53 +0200

    Feat(Stylint): Add config css modules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,13 @@
 'use strict';
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-prettier/recommended',
+  ],
   rules: {
     'custom-property-pattern': null,
     'selector-class-pattern': null,
-    // `composes` is a CSS Modules property, not standard CSS.
-    'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
   },
 };
